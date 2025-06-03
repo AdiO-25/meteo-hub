30d05c37624b2e87cba11d46d164076473684e92
AdiO-25 <adina.oberlaender@gmail.com>
2025-06-03 21:55:05 +0200

    JS updated to show search city temperature

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+function updateCityTemperature(response) {
+  let currentTemperatureNumber = document.querySelector(
+    "#current-temperature-number"
+  );
+  let temperatureElement = response.data.temperature.current;
+  currentTemperatureNumber.innerHTML = Math.round(temperatureElement);
+  let cityResult = document.querySelector("#city-result");
+  cityResult.innerHTML = response.data.city;
+}
+
+function showCurrentTemperatur(city) {
+  let apiKey = "ffda897ab634820409o4f30t6c83a646";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units={metric}`;
+  axios.get(apiURL).then(updateCityTemperature);
+}
+
 function showSearchCity(event) {
   event.preventDefault();
   let cityElement = document.querySelector("#city-input");
-  let cityResult = document.querySelector("#city-result");
-  cityResult.innerHTML = cityElement.value;
+
+  showCurrentTemperatur(cityElement.value);
 }
 
 let searchFormElement = document.querySelector("#city-form");
-searchFormElement - addEventListener("submit", showSearchCity);
+searchFormElement.addEventListener("submit", showSearchCity);
