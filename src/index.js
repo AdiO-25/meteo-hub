470fbcfd5e253ff95855d9a3f1478d3774b91ae3
AdiO-25 <adina.oberlaender@gmail.com>
2025-06-03 21:04:27 +0200

    update JS search engine

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-alert("Hello");
+function showSearchCity(event) {
+  event.preventDefault();
+  let cityElement = document.querySelector("#city-input");
+  let cityResult = document.querySelector("#city-result");
+  cityResult.innerHTML = cityElement.value;
+}
+
+let searchFormElement = document.querySelector("#city-form");
+searchFormElement - addEventListener("submit", showSearchCity);
