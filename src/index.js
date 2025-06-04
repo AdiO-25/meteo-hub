91efb176491338ea945248a691319b7839c8d7e0
AdiO-25 <adina.oberlaender@gmail.com>
2025-06-04 20:39:06 +0200

    add weather details & date time information

--- src/index.js
+++ src/index.js
@@ -1,11 +1,47 @@
 function updateCityTemperature(response) {
-  let currentTemperatureNumber = document.querySelector(
+  let currentTemperatureElement = document.querySelector(
     "#current-temperature-number"
   );
   let temperatureElement = response.data.temperature.current;
-  currentTemperatureNumber.innerHTML = Math.round(temperatureElement);
   let cityResult = document.querySelector("#city-result");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#temperature-icon");
+
+  iconElement.innerHTML = `<img
+                class="temperature-icon"
+                src="${response.data.condition.icon_url}"
+            />`;
+  timeElement.innerHTML = formatDate(date);
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  descriptionElement.innerHTML = response.data.condition.description;
+  currentTemperatureElement.innerHTML = Math.round(temperatureElement);
   cityResult.innerHTML = response.data.city;
+}
+
+function formatDate(date) {
+  let hour = date.getHours();
+  let minute = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minute < 0) {
+    minute = `0${minute}`;
+  }
+
+  return `${day} ${hour}:${minute}`;
 }
 
 function showCurrentTemperatur(city) {
